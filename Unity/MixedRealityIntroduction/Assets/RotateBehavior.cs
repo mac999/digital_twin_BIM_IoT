5c35e03ee5e418d796dfd62e294f7f950dc6b854
mac999 <laputa99999@gmail.com>
2017-12-17 20:16:35 +0900

    2

---
+++ Unity/MixedRealityIntroduction/Assets/RotateBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateBehavior : MonoBehaviour {
+    public float speed = 10f;
+
+    // Use this for initialization
+    void Start () {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+
+        // create new colors array where the colors will be created.
+        Color[] colors = new Color[vertices.Length];
+
+        for (int i = 0; i < vertices.Length; i++)
+            colors[i] = Color.Lerp(Color.red, Color.green, vertices[i].y);
+
+        // assign the array of colors to the Mesh.
+        mesh.colors = colors;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+    }
+}
