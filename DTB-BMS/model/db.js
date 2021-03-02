fd3dbbf6cc240bf450d075aca9f02fd33ebb39f9
Tae wook <laputa99999@gmail.com>
2021-03-02 14:41:12 -0500

    Add files via upload

--- DTB-BMS/model/db.js
+++ DTB-BMS/model/db.js
@@ -1,31 +1,18 @@
-// var mongoClient = require("mongodb").MongoClient;
-const mongoose = require('mongoose'); // import {mongoose} from 'mongoose';
+const mongoose = require('mongoose'); 
 
 var Schema = mongoose.Schema, ObjectId = Schema.ObjectId;
 
-/* module.exports = {
-    Sensor: sensor
-}; */
-
-function callback(err, data) {
-	console.log('kitti');
+function callbackRecrod(err, data) {
 	console.log(data);
 }
 
-function callback2(err, data) {
-	console.log('sensor');
-	console.log(data);
-}
-
-class IoTDatabase {
+class IoTDatabase {	
     async connect() {
 		var url = "mongodb://localhost:27017/sensor";  // user:pwd@localhost:
 
 		try {
 			await mongoose.connect(url);
 			console.log("Connected to Database");
-
-			// this.dumpSensorData();
 		} catch(err) {
 			console.log(err);
 		}
@@ -33,34 +20,20 @@ class IoTDatabase {
 
     async dumpSensorData() {
 		try {
-			const kittySchema = new mongoose.Schema({
-			  name: String
-			});
-			const Kitten = mongoose.model('Kitten', kittySchema);
-			const silence = new Kitten({ name: 'Silence' });
-			const fluffy = new Kitten({ name: 'fluffy' });
-			silence.save(function (err, fluffy) { });
-			fluffy.save(function (err, fluffy) { });
-
-			await Kitten.find({ name: /^fluff/ }, callback);
-
-			const SensorSchema = new Schema({
-			    // _id			: ObjectId, // a product references a category _id with type ObjectId
+			const RecordSchema = new Schema({
+			    _id		: ObjectId, 
 			    sensor 	: String,
 			    area 	: String,
 			    date 	: String,
-			    value	: Number,
+			    value	: Number
 			    // _msgid	: String
 			});
-			const sensor = mongoose.model('record', SensorSchema);
-			const temp1 = new sensor({ sensor: 'temp', area: 'Skinner', date: '2021-1-26', value: '29.0' });
-			const temp2 = new sensor({ sensor: 'temp', area: 'Skinner', date: '2021-1-26', value: '29.2' });
-			temp1.save(function (err, temp1) { });
-			temp2.save(function (err, temp2) { });
+			const record = mongoose.model('record', RecordSchema);
+			/* const r = new record({ sensor: 'temp', area: 'Skinner', date: '2021-1-26', value: '29.0' });
+			r.save(function (err, r) { }); */
 
-			await sensor.find({ sensor: /^temp/ }, callback2);
-
-			console.log("dump data");
+			console.log("Dump IoT dataset");
+			await record.find({sensor: /^temp/}, callbackRecrod).limit(3);
 		} catch(err) {
 			console.log(err);
 		}
