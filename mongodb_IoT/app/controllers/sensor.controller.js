2b791e2e64536e114c374356a53308ab67e21e4e
Tae wook <laputa99999@gmail.com>
2021-03-05 11:55:32 -0500

    Add files via upload

--- mongodb_IoT/app/controllers/sensor.controller.js
+++ mongodb_IoT/app/controllers/sensor.controller.js
@@ -1,3 +1,8 @@
+// Title: MongoDB for IoT connection
+// Developer: Taewook Kang
+// Email: laputa99999@gmail.com
+// Created date: 2021/2/26
+// 
 const Record = require('../models/sensor.model.js');
 
 // Create and Save a new record
@@ -31,19 +36,36 @@ exports.create = (req, res) => {
 
 // Retrieve and return all records from the database.
 exports.findAll = (req, res) => {
-    Record.find()
-    .then(records => {
-        res.send(records);
-    }).catch(err => {
+    console.log(req.query);
+    // ex) http://localhost:4000/sensors?limit=5&date=2021 
+    //     req.query.date;
+    var query = {};
+    if(typeof req.query.date != 'undefined') {
+        query = {date: {$gt: req.query.date}};  // {date: {$gt: '2021-2-3-17:32'}}
+    }
+
+    try {
+        if(typeof req.query.limit != 'undefined') {
+            Record.find(query).limit(parseInt(req.query.limit)).then(records => {
+                res.send(records);
+            });
+        }
+        else {
+            Record.find(query).then(records => {
+                res.send(records);
+            });
+        }
+    }
+    catch(err) {
         res.status(500).send({
             message: err.message || "Some error occurred while retrieving Record."
         });
-    });
+    }
 };
 
 // Find a record with a _id
 exports.findOne = (req, res) => {
-    Record.findById(req.params._id)
+    Record.findById(req.params.sensorId)
     .then(record => {
         if(!record) {
             return res.status(404).send({
@@ -65,6 +87,8 @@ exports.findOne = (req, res) => {
 
 // Update a record identified by the _id in the request
 exports.update = (req, res) => {
+    console.log('update ' + req.params.sensorId);
+
     // Validate Request
     if(!req.body.sensor) {
         return res.status(400).send({
@@ -73,34 +97,37 @@ exports.update = (req, res) => {
     }
 
     // Find record and update it with the request body
-    Record.findByIdAndUpdate(req.params._id, {
-        title: req.body.sensor,
+    Record.findByIdAndUpdate(req.params.sensorId, {
+        _id: req.body._id,
+        sensor: req.body.sensor,
         area: req.body.area,
         date: req.body.date, 
-        value: req.body.value
+        value: req.body.value,
+        _msgid: req.body._msgid
     }, {new: true})
     .then(record => {
         if(!record) {
             return res.status(404).send({
-                message: "Record not found with id " + req.params._id
+                message: "Record not found with id " + req.params.sensorId
             });
         }
         res.send(record);
     }).catch(err => {
         if(err.kind === 'ObjectId') {
             return res.status(404).send({
-                message: "Record not found with id " + req.params._id
+                message: "Record not found with id " + req.params.sensorId
             });                
         }
         return res.status(500).send({
-            message: "Error updating Record with id " + req.params._id
+            message: "Error updating Record with id " + req.params.sensorId
         });
     });
 };
 
 // Delete a record with the specified _id in the request
 exports.delete = (req, res) => {
-    Record.findByIdAndRemove(req.params._id)
+    console.log('delete ' + req.params.sensorId);        
+    Record.findByIdAndRemove(req.params.sensorId)
     .then(record => {
         if(!record) {
             return res.status(404).send({
